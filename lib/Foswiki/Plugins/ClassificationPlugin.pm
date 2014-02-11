a87a2f62e123ab1dcd3ac7654b0601d292d33af4
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2014-02-11 07:32:52 +0000

    Item12743: preparing new release
    git-svn-id: http://svn.foswiki.org/trunk/ClassificationPlugin@17272 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/ClassificationPlugin.pm
+++ lib/Foswiki/Plugins/ClassificationPlugin.pm
@@ -1,6 +1,6 @@
 # Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
 #
-# Copyright (C) 2006-2013 Michael Daum http://michaeldaumconsulting.com
+# Copyright (C) 2006-2014 Michael Daum http://michaeldaumconsulting.com
 # 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -17,8 +17,8 @@ use strict;
 use warnings;
 use Foswiki::Contrib::DBCacheContrib::Search ();
 
-our $VERSION = '2.00';
-our $RELEASE = '2.00';
+our $VERSION = '2.10';
+our $RELEASE = '2.10';
 our $NO_PREFS_IN_TOPIC = 1;
 our $SHORTDESCRIPTION = 'A topic classification plugin and application';
 
--- pub/System/ClassificationPlugin/jquery.hierarchy.uncompressed.js
+++ pub/System/ClassificationPlugin/jquery.hierarchy.uncompressed.js
@@ -1,7 +1,7 @@
 /*
  * jQuery hierarchy plugin 0.10
  *
- * Copyright (c) 2013 Your Name http://michaeldaumconsulting.com
+ * Copyright (c) 2013-2014 Michael Daum http://michaeldaumconsulting.com
  *
  * Dual licensed under the MIT and GPL licenses:
  *   http://www.opensource.org/licenses/mit-license.php
