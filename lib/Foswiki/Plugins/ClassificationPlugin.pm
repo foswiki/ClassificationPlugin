e0d97bee79ab1450874a323e96d8962c6f3bf702
MichaelDaum <github@michaeldaumconsulting.com>
2015-02-27 16:47:36 +0100

    Item13273: preparing release

--- lib/Foswiki/Plugins/ClassificationPlugin.pm
+++ lib/Foswiki/Plugins/ClassificationPlugin.pm
@@ -1,6 +1,6 @@
 # Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
 #
-# Copyright (C) 2006-2014 Michael Daum http://michaeldaumconsulting.com
+# Copyright (C) 2006-2015 Michael Daum http://michaeldaumconsulting.com
 # 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -31,8 +31,8 @@ BEGIN {
     }
 }
 
-our $VERSION = '3.99_002';
-our $RELEASE = '3.99_002';
+our $VERSION = '4.00';
+our $RELEASE = '27 Feb 2015';
 our $NO_PREFS_IN_TOPIC = 1;
 our $SHORTDESCRIPTION = 'A topic classification plugin and application';
 
--- pub/System/ClassificationPlugin/jquery.tagselector.uncompressed.js
+++ pub/System/ClassificationPlugin/jquery.tagselector.uncompressed.js
@@ -1,7 +1,7 @@
 /*
  * jQuery TagSelector plugin 1.1
  *
- * Copyright (c) 2008-2014 Michael Daum http://michaeldaumconsulting.com
+ * Copyright (c) 2008-2015 Michael Daum http://michaeldaumconsulting.com
  *
  * Dual licensed under the MIT and GPL licenses:
  *   http://www.opensource.org/licenses/mit-license.php
